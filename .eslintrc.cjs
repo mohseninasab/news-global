a4f9cfb43edfb32febb71f0a7ebe9a2fdd5bd20a
Mehdi Mohseni Nasab <m.mohseninasab@rahkar.co>
2024-05-07 16:41:42 +0330

    🎨 Add Prettier formater and inforce it by Eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
+  settings: { react: { version: 'detect' } },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  }
+};
