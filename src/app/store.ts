4d748ffa6cad7a9b4f945074a6796186b1976ad4
Mehdi Mohseni Nasab <m.mohseninasab@rahkar.co>
2024-05-09 20:31:39 +0330

    ✨ add Guardian api slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { newsApi } from 'src/services/newsApi';
+import { newsApi } from 'src/services/NewsApi';
+import { guardian } from 'src/services/Guardian';
+import { setupListeners } from '@reduxjs/toolkit/query/react';
 
 export const store = configureStore({
   reducer: {
     [newsApi.reducerPath]: newsApi.reducer,
+    [guardian.reducerPath]: guardian.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(newsApi.middleware),
+    getDefaultMiddleware().concat(newsApi.middleware, guardian.middleware),
 });
+
+setupListeners(store.dispatch);
